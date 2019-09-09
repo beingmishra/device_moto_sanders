d918d44c80c06dde55576e27b1bac3333a0c9051
Rahul <Rahul731999@gmail.com>
2019-09-09 18:13:22 +0000

    sanders: Use AOSP wlan

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -244,6 +244,7 @@ TARGET_USES_MKE2FS := true
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_HAS_QCOM_WLAN_SDK          := true
 BOARD_WLAN_DEVICE                := qcwcn
+BOARD_USES_AOSP_WLAN_HAL         := true
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
