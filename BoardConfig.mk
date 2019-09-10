6c1790f8eb60e4b747bfaaa19b983778ee465bca
Rahul <Rahul731999@gmail.com>
2019-09-10 12:10:13 +0000

    nuke sepolicy fully

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -223,8 +223,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/minimal
 
 # Thermal
